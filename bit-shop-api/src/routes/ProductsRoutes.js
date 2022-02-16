519165ae3ac88a6faa2654fa07252196cff71289
TzStrikerYT <maicol.asus@gmail.com>
2022-02-15 19:47:13 -0500

    [add] add update products

--- bit-shop-api/src/routes/ProductsRoutes.js
+++ bit-shop-api/src/routes/ProductsRoutes.js
@@ -8,5 +8,7 @@ const productsControllers = require("../controllers/ProductsController");
 router.get("/get-products", productsControllers.getProducts);
 router.post("/create-product", productsControllers.createProducts);
 router.put("/update-product/:productId", productsControllers.updateProducts)
+router.get("/get-one-product/:productId", productsControllers.getOneProduct)
+router.delete("/delete-product/:productId", productsControllers.deleteProduct)
 
 module.exports = router;
