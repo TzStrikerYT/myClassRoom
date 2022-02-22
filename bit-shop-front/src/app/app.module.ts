903815e7474f0c9410aa8fae40d2845d8236a23b
TzStrikerYT <maicol.asus@gmail.com>
2022-02-22 18:41:20 -0500

    [add] add angular base project

--- bit-shop-front/src/app/app.module.ts
+++ bit-shop-front/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
