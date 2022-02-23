19142e6316ce6e4b2ff4c9d78db3873b6017cd30
TzStrikerYT <maicol.asus@gmail.com>
2022-02-22 20:02:44 -0500

    [add] add angular base project

--- bit-shop-front/src/app/components/home/home.component.ts
+++ bit-shop-front/src/app/components/home/home.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products = [
+    {
+      name: "Camiseta",
+      price: "$80.000"
+    },
+    {
+      name: "Pantalon",
+      price: "$200.000"
+    },
+    {
+      name: "Pokedex",
+      price: "$800.000"
+    },
+    {
+      name: "Bit-Vision",
+      price: "$3.000.000"
+    },
+    {
+      name: "Portatil BitSus",
+      price: "$8.000.000"
+    },
+    {
+      name: "Celular BitSung",
+      price: "$1.800.000"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
