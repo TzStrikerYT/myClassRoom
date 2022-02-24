624be3bc4e079bcc32fc76cb260152f8a3bf04f3
TzStrikerYT <maicol.asus@gmail.com>
2022-02-23 20:56:16 -0500

    [add] add partial form

--- bit-shop-front/src/app/components/login/login.component.ts
+++ bit-shop-front/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
+import { User } from '../../models/user'
 
 @Component({
   selector: 'app-login',
@@ -10,6 +12,10 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
   }
+
+  login(form: NgForm){
+    console.log(form.value)
+  }
+
 }
