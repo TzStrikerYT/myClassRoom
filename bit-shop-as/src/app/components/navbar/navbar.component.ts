de65d4074065e7c10d67321d38c47d0390d1a0d5
TzStrikerYT <maicol.asus@gmail.com>
2022-02-25 21:08:18 -0500

    [add] add base project in angular

--- bit-shop-as/src/app/components/navbar/navbar.component.ts
+++ bit-shop-as/src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  token=false
+  user='Michael Cocuy'
+
   constructor() { }
 
   ngOnInit(): void {
