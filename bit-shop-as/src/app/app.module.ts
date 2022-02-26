61ed2b31881de94c980e0b473c67655062d789ab
TzStrikerYT <maicol.asus@gmail.com>
2022-02-25 19:07:31 -0500

    [add] add base project in angular

--- bit-shop-as/src/app/app.module.ts
+++ bit-shop-as/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    HomeComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
